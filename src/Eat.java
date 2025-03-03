bb1701929d4f5f3af56dc3e76ecd616f44e1b94a
Tepanyega Samuel <72327734+Tepanyega@users.noreply.github.com>
2025-03-03 09:20:28 +0200

    Update Eat.java

--- src/Eat.java
+++ src/Eat.java
@@ -1,19 +1,8 @@
 //interfaces define a contract that multiple classes can implement,
-//can even help you achieve multiple inheritance
 public interface Eat{
 
      //abstract method, you don't have to implicitly specify the access modifier
      void eatFood();
      void eatingCompleted();
-
-     //implementation of the eating food method
-     //this is polymorphism
      void eatingFood();
-
-     //you can only have abstract method and default method
-     //which allows you to have a method with body
-     //but don't forget to use the default key word
-
-     //Again you can have concrete methods, but these method will not be accesses in the subclasses
-
 }
