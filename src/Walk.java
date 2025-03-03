ec89d1cde797af74cf1ff7f4b872578f495b5938
Tepanyega Samuel <72327734+Tepanyega@users.noreply.github.com>
2025-03-03 09:22:55 +0200

    Update Walk.java
    interface walk update

--- src/Walk.java
+++ src/Walk.java
@@ -1,14 +1,5 @@
-//interfaces define a contract that multiple classes can implement
-//it can even help you achieve multiple inheritance
 //as java doesnt support multiple inheritance
 public interface Walk {
-
     //abstract method, you don't have to implicitly specify the access modifier
     void walking();
-
-//you can only have abstract method and default method
-//which allows you to have a method with body
-//but dont forget to use the default key word
-
-//Again you can have concrete methods, but these method will not be accesses in the subclasses
 }
